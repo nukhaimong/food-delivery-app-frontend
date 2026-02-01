f8f8c84a10ad049c874ce1c53c4b2fe7ef37eb0c
nukhaimong <nukhaimongmarma834@gmail.com>
2026-02-01 18:37:35 +0600

    initialized next js

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Food Delivery App. Choose your meal and enjoy</h1>
+    </div>
+  );
+}
