b3b9fa0c6b95c34acc6994952fbac4532dd1122d
nukhaimong <nukhaimongmarma834@gmail.com>
2026-02-20 13:18:21 +0600

    fixed baseURL again for production

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.APP_URL,
+  baseURL: 'https://food-delivery-app-backend-58qb.onrender.com',
 });
