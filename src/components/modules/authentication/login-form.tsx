5533c5b03ca157e2f61473d93855957e1024242f
nukhaimong <nukhaimongmarma834@gmail.com>
2026-02-20 15:00:56 +0600

    add credentials in login and sign up

--- src/components/modules/authentication/login-form.tsx
+++ src/components/modules/authentication/login-form.tsx
@@ -45,6 +45,7 @@ export function LoginForm({ ...props }: React.ComponentProps<'div'>) {
           ...value,
           callbackURL: 'https://food-delivery-app-frontend-umber.vercel.app/',
           fetchOptions: {
+            credentials: 'include',
             onSuccess: () => {
               useCartStore.persist.clearStorage();
             },
