118eabff9470c41235b5aa7739f52de3870f1c1f
nukhaimong <nukhaimongmarma834@gmail.com>
2026-02-22 23:13:51 +0600

    add something in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: `https://food-delivery-app-backend-58qb.onrender.com/api/auth/:path*`,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
