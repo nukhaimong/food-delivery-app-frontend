80d98e2936e1733d3cf4ad89b36af9d2c88b0d60
nukhaimong <nukhaimongmarma834@gmail.com>
2026-02-23 01:35:58 +0600

    testing

--- src/app/(common layout)/page.tsx
+++ src/app/(common layout)/page.tsx
@@ -5,8 +5,13 @@ import TopRestaurants from '@/components/modules/homepage/topRestaurants';
 import { categoryService } from '@/services/category.service';
 import { mealService } from '@/services/meal.service';
 import { providerService } from '@/services/provider.service';
+import { cookies } from 'next/headers';
 
 export default async function Home() {
+  // Get cookies from the browser's request
+  const cookieStore = await cookies();
+  const cookieHeader = cookieStore.toString();
+
   const { data } = await categoryService.getCategory();
   const { data: mealData } = await mealService.getMeal();
   const { data: providers } = await providerService.getProvidersProfiles();
@@ -14,6 +19,18 @@ export default async function Home() {
   const providersData = providers?.allProvidersProfiles;
   const categories = data?.category;
   const meals = mealData?.meals;
+
+  const sessionRes = await fetch(
+    'https://food-delivery-app-backend-58qb.onrender.com/api/auth/get-session',
+    {
+      headers: {
+        Cookie: cookieHeader, // Manually forward cookies
+      },
+      cache: 'no-store',
+    },
+  );
+  const session = await sessionRes.json();
+  console.log('Session:', session);
 
   return (
     <div className="max-w-7xl mx-auto">
